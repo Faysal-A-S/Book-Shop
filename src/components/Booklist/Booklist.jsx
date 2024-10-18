f7c7f6e03574cb4cddb734c91c39be0af324f689
Faysal Ahmed <hl.faysal1@gmail.com>
2024-10-19 00:29:41 +0600

    feat: pagination and books add

--- src/components/Booklist/Booklist.jsx
+++ src/components/Booklist/Booklist.jsx
@@ -1,8 +1,28 @@
 
+import { useState } from "react"
+import Pagination from "../Resources/Pagination/Pagination"
+import BookCard from "./BookCard/BookCard"
 import classes from "./Booklist.module.css"
-const Booklist = () => {
+const Booklist = ({data,currentPage,setCurrentPage}) => {
+  const count=data?.count ||1
+  const totalPage = Math.ceil(count/32)
+  const storedList = localStorage.getItem("wishlist")
+  let storedObject = storedList ? JSON.parse(storedList) : [];
+  const [wishlist,setWishlist]=useState([storedObject])
   return (
-    <div className={classes.container}>Booklist</div>
+    <div className={classes.container}>
+      <div className={classes.pagination}>
+        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPage={totalPage}/>
+      </div>
+    <div className={classes.wrapper}>
+    {data?.results &&  data?.results?.map((book)=>{
+      return<BookCard key={book.id} data={book} wishlist={wishlist} setWishlist={setWishlist}/>
+     }) }
+    </div>
+    <div className={classes.pagination}>
+        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPage={totalPage}/>
+      </div>
+    </div>
   )
 }
 
