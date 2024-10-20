2375bf958eec097e6e9e5923942aa4e46ba7c2ff
Faysal Ahmed <faysal15-1655@diu.edu.bd>
2024-10-20 17:07:40 +0600

    feat: link color add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom"
-import classes from "./Navbar.module.css"
+import { Link, useLocation } from "react-router-dom";
+import classes from "./Navbar.module.css";
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <div className={classes.container}>
-      <Link to=""><h2>Book Store</h2></Link>
-      <Link to="/wishlist"> <p>WishList</p></Link>
+      <Link to="">
+        <h2>Book Store</h2>
+      </Link>
+      <Link to="/wishlist">
+        <p
+          className={`${location.pathname !== "/wishlist" ? classes.link : classes.selected}`}
+        >
+          WishList
+        </p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
