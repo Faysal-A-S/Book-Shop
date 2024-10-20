a4faead9b961111da26c2f520467166f3e2cdfa4
Faysal Ahmed <faysal15-1655@diu.edu.bd>
2024-10-20 17:11:12 +0600

    refactor: spacing change

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,41 +1,44 @@
-import Uppersection from "../components/Uppersection/Uppersection"
-import Booklist from "../components/Booklist/Booklist"
-import { useEffect, useState } from "react"
-import { apiV1 } from './../api/apiUrl';
+import Uppersection from "../components/Uppersection/Uppersection";
+import Booklist from "../components/Booklist/Booklist";
+import { useEffect, useState } from "react";
+import { apiV1 } from "./../api/apiUrl";
 import { useDebounce } from "../components/Resources/useDebounce";
 
 const Homepage = () => {
-  const [bookData,setBookData]=useState({})
+  const [bookData, setBookData] = useState({});
   const [title, setTitle] = useState(() => {
     const savedSearch = localStorage.getItem("search");
-    return savedSearch || ""; 
+    return savedSearch || "";
   });
-  const [currentPage,setCurrentPage]=useState(1)
-  const debouncedTitle=useDebounce(title)
-  const [loading,setLoading]=useState(true)
+  const [currentPage, setCurrentPage] = useState(1);
+  const debouncedTitle = useDebounce(title);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    const fetchData =async()=>{
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(`${apiV1}/books/?page=${currentPage}&search=${debouncedTitle}`,{
-          method:"GET",
-          headers:{
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
+        const response = await fetch(
+          `${apiV1}/books/?page=${currentPage}&search=${debouncedTitle}`,
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
           }
-        })
-        const data = await response.json()
-        if(response.ok){
-          setBookData(data)
-          setLoading(false)
+        );
+        const data = await response.json();
+        if (response.ok) {
+          setBookData(data);
+          setLoading(false);
         }
       } catch (error) {
-        console.log(error)
-        setBookData([])
+        console.log(error);
+        setBookData([]);
       }
-    }
-    fetchData()
-  },[currentPage,debouncedTitle])
+    };
+    fetchData();
+  }, [currentPage, debouncedTitle]);
   useEffect(() => {
     const savedSearch = localStorage.getItem("search");
     if (savedSearch) {
@@ -44,14 +47,19 @@ const Homepage = () => {
   }, []);
   return (
     <div>
-      <Uppersection setTitle={setTitle} pageTitle="Books" title={title}/>
+      <Uppersection setTitle={setTitle} pageTitle="Books" title={title} />
 
-     {
-      loading? <p>Loading please wait.....</p>: <Booklist data={bookData} setCurrentPage={setCurrentPage} currentPage={currentPage}/>
-     }
-
+      {loading ? (
+        <p>Loading please wait.....</p>
+      ) : (
+        <Booklist
+          data={bookData}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
