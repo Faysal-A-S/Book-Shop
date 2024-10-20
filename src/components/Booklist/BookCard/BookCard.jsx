7cefdd180388232439ff6a7c15d8378f1c04680d
Faysal Ahmed <faysal15-1655@diu.edu.bd>
2024-10-20 16:59:44 +0600

    feat: link add

--- src/components/Booklist/BookCard/BookCard.jsx
+++ src/components/Booklist/BookCard/BookCard.jsx
@@ -1,17 +1,18 @@
-
 import { useEffect, useState } from "react";
-import classes from "./BookCard.module.css"
+import classes from "./BookCard.module.css";
 import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
+const BookCard = ({ data, wishlist, setWishlist }) => {
+  const { authors, id, title, formats } = data;
+  const [isListed, setIslisted] = useState(false);
 
-const BookCard = ({data,wishlist,setWishlist}) => {
+  const handleWishList = (event) => {
+    // Stop the click event from bubbling up to the Link
+    event.preventDefault();
+    event.stopPropagation();
 
-
-const {authors,id,title,formats}=data
-const [isListed,setIslisted]=useState(false)
-
-  const handleWishList = () => {
-    if (wishlist!==null && wishlist.length > 0) {
+    if (wishlist !== null && wishlist.length > 0) {
       const isInWishlist = wishlist.some((item) => item.id === data.id);
       const updatedWishlist = isInWishlist
         ? wishlist.filter((item) => item.id !== data.id)
@@ -21,35 +22,46 @@ const [isListed,setIslisted]=useState(false)
       setWishlist([data]);
     }
   };
-  
+
   useEffect(() => {
-    if (wishlist!==null && wishlist.length > 0) {
+    if (wishlist !== null && wishlist.length > 0) {
       const isInWishlist = wishlist.some((wish) => wish.id === id);
       setIslisted(isInWishlist);
     } else {
       setIslisted(false);
     }
   }, [id, wishlist]);
-  
+
   useEffect(() => {
-    if (wishlist!==null && wishlist.length > 0) {
-      const cleanedList = wishlist.filter((list) => list !== null && list !== undefined && Object.keys(list).length > 0);
-      localStorage.setItem('wishlist', JSON.stringify(cleanedList));
+    if (wishlist !== null && wishlist.length > 0) {
+      const cleanedList = wishlist.filter(
+        (list) =>
+          list !== null && list !== undefined && Object.keys(list).length > 0
+      );
+      localStorage.setItem("wishlist", JSON.stringify(cleanedList));
     }
   }, [wishlist]);
-  return (
-    <div className={classes.container}>
-      <p>Book No: {id}</p>
-      <div className={classes.imageContainer}>
-        <img src={formats["image/jpeg"]} alt="" />
-      </div>
-      <FaHeart className={classes.heartIcon} onClick={handleWishList} fill={isListed ? "rgb(238, 124, 124)":"currentColor"}/>
-      <div className={classes.details}>
-        <h2>{title}</h2>
-       {authors[0]?.name && <p>By  {authors[0]?.name}</p>}
-      </div>
-    </div>
-  )
-}
 
-export default BookCard
+  return (
+    <Link to={`/books/${id}`} className={classes.link}>
+      <div className={classes.container}>
+        <p>Book No: {id}</p>
+        <div className={classes.imageContainer}>
+          <img src={formats["image/jpeg"]} alt="" />
+        </div>
+        <button onClick={(e) => handleWishList(e)}>
+          <FaHeart
+            className={classes.heartIcon}
+            fill={isListed ? "rgb(238, 124, 124)" : "currentColor"}
+          />
+        </button>
+        <div className={classes.details}>
+          <h2>{title}</h2>
+          {authors[0]?.name && <p>By {authors[0]?.name}</p>}
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default BookCard;
