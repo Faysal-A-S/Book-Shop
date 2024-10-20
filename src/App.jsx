f34ed901acfbcedd2d5518a6e91f42e486775e39
Faysal Ahmed <faysal15-1655@diu.edu.bd>
2024-10-20 16:46:57 +0600

    feat: single book link add

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-
-import { Route, Routes,BrowserRouter as Router } from 'react-router-dom'
-import './App.css'
-import Homepage from './pages/Homepage'
-import Navbar from './components/Navbar/Navbar'
-import WishListPage from './pages/WishListPage'
-
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar/Navbar";
+import WishListPage from "./pages/WishListPage";
+import Bookpage from "./pages/Bookpage";
 
 function App() {
-
-
   return (
     <>
-       
- 
-   <Router>
-   
-   <Navbar/>
-<div className='container'>
-<Routes >
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='/wishlist' element={<WishListPage/>}/>
-    </Routes>
-</div>
-
-  </Router>
-
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/wishlist" element={<WishListPage />} />
+            <Route path="/books/:id" element={<Bookpage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
