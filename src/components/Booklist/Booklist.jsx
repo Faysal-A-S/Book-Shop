11b2003df0f2d76c17b5de8790bf5dcb50441245
Faysal Ahmed <faysal15-1655@diu.edu.bd>
2024-10-20 17:07:20 +0600

    fix: local storage initial data fix

--- src/components/Booklist/Booklist.jsx
+++ src/components/Booklist/Booklist.jsx
@@ -1,29 +1,44 @@
-
-import { useState } from "react"
-import Pagination from "../Resources/Pagination/Pagination"
-import BookCard from "./BookCard/BookCard"
-import classes from "./Booklist.module.css"
-const Booklist = ({data,currentPage,setCurrentPage}) => {
-  const count=data?.count ||1
-  const totalPage = Math.ceil(count/32)
-  const storedList = localStorage.getItem("wishlist")
+import { useState } from "react";
+import Pagination from "../Resources/Pagination/Pagination";
+import BookCard from "./BookCard/BookCard";
+import classes from "./Booklist.module.css";
+const Booklist = ({ data, currentPage, setCurrentPage }) => {
+  const count = data?.count || 1;
+  const totalPage = Math.ceil(count / 32);
+  const storedList = localStorage.getItem("wishlist");
   let storedObject = storedList ? JSON.parse(storedList) : [];
-  const [wishlist,setWishlist]=useState([storedObject])
+  const [wishlist, setWishlist] = useState(storedObject);
   return (
     <div className={classes.container}>
       <div className={classes.pagination}>
-        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPage={totalPage}/>
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalPage={totalPage}
+        />
       </div>
-    <div className={classes.wrapper}>
-    {data?.results &&  data?.results?.map((book)=>{
-      return<BookCard key={book.id} data={book} wishlist={wishlist} setWishlist={setWishlist}/>
-     }) }
-    </div>
-    <div className={classes.pagination}>
-        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPage={totalPage}/>
+      <div className={classes.wrapper}>
+        {data?.results &&
+          data?.results?.map((book) => {
+            return (
+              <BookCard
+                key={book.id}
+                data={book}
+                wishlist={wishlist}
+                setWishlist={setWishlist}
+              />
+            );
+          })}
+      </div>
+      <div className={classes.pagination}>
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalPage={totalPage}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Booklist
+export default Booklist;
